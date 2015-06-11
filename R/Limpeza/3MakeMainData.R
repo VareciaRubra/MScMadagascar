4e7f32c3af932d9d3f7ea635c367259ae33abe81
Anna Penna <annapontopenna@gmail.com>
2015-06-11 18:27:52 -0300

    fixed main data

--- R/Limpeza/3MakeMainData.R
+++ R/Limpeza/3MakeMainData.R
@@ -1,8 +1,9 @@
 ################ ?Organizando tudo em listas por ranking taxonomico ##############
 
 current.data <- raw.main.data$Eulemur_albifrons
-makeMainData <- function (current.data, final = FALSE) {
-  x = vector("list", 13)
+makeMainData <- function (current.data, compare.size = FALSE, final = FALSE, ) {
+  x = vector("list", 16 )
+  x[1:11] <- NA
   x[[1]] <- dplyr::select(current.data, c(Arquivo:Take)) # pegando as info de cada replica
   x[[2]] <- dplyr::select(current.data, c(IS_PM:BA_OPI)) # pegando as ed de cada replica
   x[[3]] <- base::unique(select(current.data, c(Arquivo:Data_dado))) # pegando as info únicas de cada indivíduo
@@ -27,52 +28,58 @@ makeMainData <- function (current.data, final = FALSE) {
   x[[11]][[4]] <- if(x[[9]]>20) var(x[[6]]) else matrix(data = NA, nrow = 39, ncol = 39) 
   names(x[[11]]) <- 'matrix'
   names(x[[11]])[1:4] <- c('cov','cor', 'cov.sizeless', 'cov.log')
-if (compare.size == TRUE)
-  {x[[12]] <- ldply(.data = x[[11]][-2], .fun = MeanMatrixStatistics, parallel = TRUE)
-    names (x[[12]]) <- 'mean.mx.stats'
-   x[[13]] <- vector("numeric", 4)
-   x[[13]][[1]]<- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
+    
+  x[[13]] <- vector("list", 4)
+  names(x[[13]])[1:4] <-  c('cov.RS','cor.Mt', 'cov.sizeless.RS', 'cov.log.RS')
+  if (compare.size == TRUE)
+  {x[[12]] <- if(x[[9]]>40) ldply(.data = x[[11]][-2], .fun = MeanMatrixStatistics, parallel = TRUE, full.results = FALSE) else NA
+   x[[13]][[1]]<- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
    x[[13]][[2]]<- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
-   x[[13]][[3]]<- if(x[[9]]>20) BootstrapRep( x[[5]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
+   x[[13]][[3]]<- if(x[[9]]>20) BootstrapRep( x[[5]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
    x[[13]][[4]]<- if(x[[9]]>20) BootstrapRep( x[[6]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
-   names(x)[13] <- c('BootsRep')
-   #x[[13]] <- t(x[[13]])
-   #dimnames(x[[13]]) <- c('cov.RS','cor.Mt', 'cov.sizeless.RS', 'cov.log.RS')
-   x[[14]] <- RandomSkewers(cov.x = x[[11]][-2], num.vectors = 1000, repeat.vector = x[[13]][-2]) 
-   names(x)[[14]] <- 'rs.size.comparisson'
-   return(x)}
-  if (final == TRUE)
-  {x[[15]] <- vector("list", 5)
-   x[[15]][[1]] <- if(x[[9]]>20) Rarefaction(x[[4]], RandomSkewers, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-   x[[15]][[2]] <- if(x[[9]]>20) Rarefaction(x[[4]], KrzCor, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-   x[[15]][[3]] <- if(x[[9]]>20) Rarefaction(x[[4]], PCAsimilarity, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-   x[[15]][[4]] <- if(x[[9]]>20) Rarefaction(x[[4]], MatrixCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-   x[[15]][[5]] <- if(x[[9]]>20) Rarefaction(x[[4]], KrzCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-   names(x)[15] <- c('rarefaction')
-   names(x[[15]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
-   x[[16]] <- vector("list", 5)
-   x[[16]][[1]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
-   x[[16]][[2]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, parallel = T) else NA
-   x[[16]][[3]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = PCAsimilarity, iterations = 1000, parallel = T) else NA
-   x[[16]][[4]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
-   x[[16]][[5]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
-   names(x)[16] <- c('BootsRep')
-   names(x[[16]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
-   return(x)}
+   
+   x[[14]] <- RandomSkewers(cov.x = x[[11]][-2], num.vectors = 1000, repeat.vector = unlist(x[[13]][-2]) ) 
+   names (x[12:14]) <- c('mean.mx.stats', 'BootsRep', 'rs.size.comparisson')
+  }   
+  names (x[12:14]) <- c('mean.mx.stats', 'BootsRep', 'rs.size.comparisson')
   
-  else return(x)
+  x[[15]] <- vector("list", 5)
+  x[[16]] <- vector("list", 5)
+  if (final == TRUE)
+  {
+    x[[15]][[1]] <- if(x[[9]]>20) Rarefaction(x[[4]], RandomSkewers, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[2]] <- if(x[[9]]>20) Rarefaction(x[[4]], KrzCor, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[3]] <- if(x[[9]]>20) Rarefaction(x[[4]], PCAsimilarity, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[4]] <- if(x[[9]]>20) Rarefaction(x[[4]], MatrixCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[5]] <- if(x[[9]]>20) Rarefaction(x[[4]], KrzCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    
+    
+    x[[16]][[1]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
+    x[[16]][[2]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, parallel = T) else NA
+    x[[16]][[3]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = PCAsimilarity, iterations = 1000, parallel = T) else NA
+    x[[16]][[4]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+    x[[16]][[5]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+  }  
+  
+  names(x)[15] <- c('rarefaction')
+  names(x[[15]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
+  names(x)[16] <- c('BootsRep')
+  names(x[[16]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
+  
+  return(x)
   
 }
 
-makeMainData(raw.main.data$Eulemur_albifrons, final = TRUE)
+main.data.albifrons<- makeMainData(raw.main.data$Eulemur_albifrons, compare.size = TRUE, final = TRUE)
+main.data<- llply(raw.main.data, makeMainData, compare.size = TRUE, final = TRUE, .progress = 'text')
+all.main.data <- llply(all.raw.main.data,  final = TRUE, makeMainData, .progress = 'text')
 
-main.data <- llply(raw.main.data, makeMainData, final = TRUE, .progress = 'text')
-all.main.data <- llply(all.raw.main.data,  final = FALSE, makeMainData)
+#Hapalemur.image <- list (main.data = main.data,   
+#                      all.main.data = all.main.data)
 
 Eulemur.image <- list (main.data = main.data,   
-                         all.main.data = all.main.data)
+                       all.main.data = all.main.data)
 
 save(Eulemur.image,
-    file = arquivo.saida)
-
+     file = "attaches/Eulemur.image")
 
