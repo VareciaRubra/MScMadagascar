b2c6f414edbbdeb4b9adf42f7633ddbb2a6a52e0
Anna Penna <annapontopenna@gmail.com>
2015-06-13 19:22:49 -0300

    função para plotar matrizes

--- R/Limpeza/4Models.R
+++ R/Limpeza/4Models.R
@@ -1,13 +1,25 @@
-###################### testando modelos para possiveis correções de efeitos fixos ##################
+
+#############################################
+############# PRIMEIRA PARTE ################
+##### testando modelos de efeitos fixos #####
+#############################################
 options(contrasts=c("contr.sum", "contr.poly"))
 
 current.data <- main.data$Propithecus_verreauxi
   y = vector("list", 4)
-  y[[1]] <- manova(as.matrix(current.data$ed) ~ Museu, data = as.data.frame(current.data$info))
-  y[[2]] <- Manova(y[[1]] , type=3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly"))
-  y[[3]] <- apply(as.matrix(current.data$ed), 2, function (x) return (Anova(lm(x ~ Especie, data = as.data.frame(current.data$info) ), type= 3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly" ))))
-  y[[4]] <- CalculateMatrix(y[[1]]) 
-  names(y)[1:3] <- c("fit", "multi", "uni", "cov.mx")
+  y[[1]] = vector("list", 4)
+  y[[1]]  <- manova(as.matrix(current.data$ed) ~ Museu, data = as.data.frame(current.data$info))
+  y[[1]] [2] <- Manova(y[[1]] , type=3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly"))
+  y[[1]] [3]  <- apply(as.matrix(current.data$ed), 2, function (x) return (Anova(lm(x ~ Especie, data = as.data.frame(current.data$info) ), type= 3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly" ))))
+  y[[1]] [4] <- CalculateMatrix(y[[1]]) 
+  names(y)[[1]][1:4] <- c("fit", "multi", "uni", "cov.mx")
+
+
+#############################################
+############## SEGUNDA PARTE ################
+## MATRIZ de genero: residual de especies  ##
+#############################################
+
 
 require (plotrix)
 
@@ -17,9 +29,27 @@ RandomSkewers (CalculateMatrix(y[[1]]),
 RandomSkewers (CalculateMatrix(y[[1]]), 
                var (current.data$ed))
 
-par(mfrow =c(1,2) )
-color2D.matplot (cov2cor(CalculateMatrix(y[[1]])), axes = F)
-axis(1, 1:39, rownames(cor (current.data$ed)), las = 3, cex.axis = 0.51)
-axis(2, 39:1, rownames(cor (current.data$ed)), las = 1, cex.axis = 0.51)
-color2D.matplot( cor (current.data$ed), axes = F) 
-axis(1, 1:39, rownames(cor (current.data$ed)), las = 3, cex.axis = 0.51)
+plot.matrix.cor<- function(cor.mx = NULL, main = "", title = "", brewer = "BrBG", show.values = TRUE )
+  {
+  library("RColorBrewer")
+  paleta.of.choice  <- rev(brewer.pal(11, brewer))
+  colfunc <- colorRampPalette(paleta.of.choice)
+  paleta  <- colfunc(200)
+  
+  nCores  <- length(paleta)
+  intervalMarks  <- seq(from = -1, to = 1, length.out = nCores)
+  dados<- as.vector(cor.mx)
+  intervals  <- findInterval(dados , intervalMarks, rightmost.closed = T, all.inside = T)
+  cores  <- paleta[intervals]
+  
+  mx.dimentions <- dim(cor.mx)[1]
+  
+  color2D.matplot(x = cor.mx, axes = F, cellcol = cores, show.values= show.values, vcex= 0.5, xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+  title(main = main , sub =  title)
+  axis(1, 1:mx.dimentions, rownames(cor.mx), las = 3, cex.axis = 0.6, tick = FALSE)
+  axis(2, mx.dimentions:1, rownames(cor.mx), las = 1, cex.axis = 0.6, tick = FALSE)
+  }
+  
+plot.matrix.cor(cor.mx = cov2cor(CalculateMatrix(y[[1]])) )
+
+
