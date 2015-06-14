a32eece75ca0a1df81dd71b79f2a0e37f770010f
Anna Penna <annapontopenna@gmail.com>
2015-06-13 21:45:53 -0300

    modelo e plot de mx cor

--- R/Limpeza/4Models.R
+++ R/Limpeza/4Models.R
@@ -5,13 +5,13 @@
 #############################################
 options(contrasts=c("contr.sum", "contr.poly"))
 
-current.data <- main.data$Propithecus_verreauxi
+current.data <- all.main.data$Propithecus
   y = vector("list", 4)
   y[[1]] = vector("list", 4)
-  y[[1]]  <- manova(as.matrix(current.data$ed) ~ Museu, data = as.data.frame(current.data$info))
-  y[[1]] [2] <- Manova(y[[1]] , type=3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly"))
-  y[[1]] [3]  <- apply(as.matrix(current.data$ed), 2, function (x) return (Anova(lm(x ~ Especie, data = as.data.frame(current.data$info) ), type= 3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly" ))))
-  y[[1]] [4] <- CalculateMatrix(y[[1]]) 
+  y[[1]] [[1]] <- manova(as.matrix(current.data$ed) ~ Especie, data = as.data.frame(current.data$info))
+  y[[1]] [[2]] <- Manova(y[[1]] [[1]], type=3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly"))
+  y[[1]] [[3]]  <- apply(as.matrix(current.data$ed), 2, function (x) return (Anova(lm(x ~ Especie, data = as.data.frame(current.data$info) ), type= 3, test.statistic="Wilks",icontrasts=c("contr.sum", "contr.poly" ))))
+  y[[1]] [[4]] <- CalculateMatrix(y[[1]] [[1]]) 
   names(y)[[1]][1:4] <- c("fit", "multi", "uni", "cov.mx")
 
 
