892e55c2dd9e1ed8f28d6df7dde279fefa8349a6
Anna Penna <annapontopenna@gmail.com>
2015-06-15 20:29:37 -0300

    Prolemur outliers e clean file.csv

--- R/Limpeza/2LimpezaPlots.R
+++ R/Limpeza/2LimpezaPlots.R
@@ -80,7 +80,7 @@ raw.data %>%
 ######## Biplot PC1 x PC2 ##############
 current.data <- all.main.data[[1]]
 PRCOMP <- princomp(current.data$ed) # extraindo os componentes principais a partir dos dados (medias ed)
-#PRCOMP <- princomp(data.frame(na.omit(current.data$sizeless ) ) )
+PRCOMP <- princomp(data.frame(na.omit(current.data$sizeless ) ) )
 resp <- current.data$info[current.data$info$Tombo %in% dimnames(PRCOMP$scores)[[1]], ] #respectivos dados aos participantes da PCA
 resp %<>% mutate(., PC1 = PRCOMP$scores[,1], PC2=PRCOMP$scores[,2]) 
 hulls <-ddply(resp, .(Especie), plyr::summarise, "hpc1"=PC1[chull(PC1,PC2)],
