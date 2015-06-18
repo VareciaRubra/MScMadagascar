0d5d749881ef9b6c161f766aa534b1218323fc57
Anna Penna <annapontopenna@gmail.com>
2015-06-18 16:46:16 -0300

    names matrix repetabilities liest for bootstrap rep

--- R/Limpeza/3MakeMainData.R
+++ R/Limpeza/3MakeMainData.R
@@ -1,6 +1,6 @@
 ################ ?Organizando tudo em listas por ranking taxonomico ##############
 
-current.data <- raw.main.data[[1]]
+current.data <- all.raw.main.data[[1]]
 makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, final = FALSE ) 
   {
   x = vector("list", 16 )
@@ -27,10 +27,10 @@ makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, fin
     sp.fit <-  if (specie == FALSE & sp.number > 1) manova(as.matrix(x[[4]]) ~ Especie, data = as.data.frame(x[[3]])) else NA
     gr <- if(specie == TRUE) var(x[[4]]) else CalculateMatrix(sp.fit) 
     pq <- matrix(data = NA, nrow = 39, ncol = 39) 
-  x[[11]][[1]] <- if(x[[9]]>20) gr else pq
-  x[[11]][[2]] <- if(x[[9]]>20) cov2cor(x[[11]][[1]]) else pq
-  x[[11]][[3]] <- if(x[[9]]>20) var(x[[5]]) else pq
-  x[[11]][[4]] <- if(x[[9]]>20) var(x[[6]]) else pq
+  x[[11]][[1]] <- if(x[[9]]>15) gr else pq
+  x[[11]][[2]] <- if(x[[9]]>15) cov2cor(x[[11]][[1]]) else pq
+  x[[11]][[3]] <- if(x[[9]]>15) var(x[[5]]) else pq
+  x[[11]][[4]] <- if(x[[9]]>15) var(x[[6]]) else pq
   x[[11]][[5]] <- sp.fit 
   names(x)[11] <- 'matrix'
   names(x[[11]])[1:5] <- c('cov','cor', 'cov.sizeless', 'cov.log', 'fit')
@@ -47,10 +47,11 @@ makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, fin
 #    x[[14]] <- RandomSkewers(cov.x = x[[11]][-2], num.vectors = 1000, repeat.vector = unlist(x[[13]][-2]) ) 
 #    names (x)[12:14] <- c('mean.mx.stats', 'BootsRep', 'rs.size.comparisson')
 #   }   
-  names (x)[12:14] <- c('mean.mx.stats', 'BootsRep', 'rs.size.comparisson')
+  names (x)[12:14] <- c('mean.mx.stats', 'size.mx.rep', 'rs.size.comparisson')
   
   x[[15]] <- vector("list", 5)
-  x[[16]] <- vector("list", 5)
+  x[[16]] <- vector("list", 1)
+  x[[16]][[1]] <- matrix(NA, nrow = 1, ncol= 5)
   if (final == TRUE)
   {
     #x[[15]][[1]] <- if(x[[9]]>20) Rarefaction(x[[4]], RandomSkewers, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
@@ -59,17 +60,18 @@ makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, fin
 #     x[[15]][[4]] <- if(x[[9]]>20) Rarefaction(x[[4]], MatrixCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
 #     x[[15]][[5]] <- if(x[[9]]>20) Rarefaction(x[[4]], KrzCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
         
-    x[[16]][[1]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
-#     x[[16]][[2]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, parallel = T) else NA
-#     x[[16]][[3]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = PCAsimilarity, iterations = 1000, parallel = T) else NA
-#     x[[16]][[4]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
-#     x[[16]][[5]] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+    x[[16]][[1]][1] <- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
+#     x[[16]][[1]][2] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, parallel = T) else NA
+#     x[[16]][[1]][3] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = PCAsimilarity, iterations = 1000, parallel = T) else NA
+#     x[[16]][[1]][4] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+#     x[[16]][[1]][5] <- if(x[[9]]>20) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
   }  
   
   names(x)[15] <- c('rarefaction')
   names(x[[15]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
-  names(x)[16] <- c('BootsRep')
-  names(x[[16]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
+  names(x)[16] <- c('Mx.Rep')
+  names(x[[16]])[[1]] <- c('BootsRep')
+  colnames(x[[16]][[1]])[1:5] <- c('rs', 'krz','pcas', 'cor.mantel', 'cor.krz')
   
   return(x)
   
@@ -89,7 +91,7 @@ all.main.data <- llply(all.raw.main.data, specie = TRUE, final = FALSE, makeMain
 
 #makeMainData(raw.main.data[[1]], specie = TRUE, compare.size = TRUE, final = TRUE)
 
-main.data<- llply(raw.main.data, makeMainData, specie = TRUE, compare.size = TRUE, final = TRUE, .progress = 'text')
+main.data<- llply(raw.main.data, makeMainData, specie = TRUE, compare.size = FALSE, final = TRUE, .progress = 'text')
 
 all.main.data<- llply(all.raw.main.data, makeMainData, specie = FALSE, compare.size = FALSE, final = TRUE, .progress = 'text')
 
