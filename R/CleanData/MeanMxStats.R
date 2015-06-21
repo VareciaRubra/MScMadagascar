fe44df0e4b7fcd2fe5b1ed43470a31917ec490f9
Anna Penna @Darwin <annapontopenna@gmail.com>
2015-06-22 00:17:13 +0400

    Make main data rolando pra sp mas empacou pra mesopropithecus. no erro produziu um objeto "core" bizarro outra vez que nao sei de onde veio...

--- R/CleanData/MeanMxStats.R
+++ R/CleanData/MeanMxStats.R
@@ -1,10 +1,13 @@
-cov.mx<- sp.master.main.data %>% llply(function(x) x$matrix$cov) %>% {. %*% t(.)}
-mx.rep<- sp.master.main.data %>% ldply(function(x) x$Mx.Rep$BootsRep[1]) %>% {. %*% t(.)}
-n.size<- sp.master.main.data %>% ldply(function(x) x$sample.size) %>% {. %*% t(.)}
+cov.mx <- sp.master.main.data %>% llply(function(x) x$matrix$cov)
+mx.rep <- sp.master.main.data %>% ldply(function(x) x$Mx.Rep$BootsRep[1]) 
+n.size <- sp.master.main.data %>% ldply(function(x) x$sample.size) 
 
+mask.n.size <- n.size[,2]>40
+mask.na.cov <- ldply(cov.mx, function(x) !is.na(x[1]))[,2]
+mask.rep <- !is.na(mx.rep)[,2]
 
-mask1 <- ldply(sp.master.main.data, function(x) x$sample.size >40) [,2]
-stats <- llply(sp.master.main.data, function(x) if (!is.na(x$matrix$cov[1])  ) x$matrix$cov else NA)
+distance <- llply(cov.mx[mask.na.cov], MatrixDistance, distance = "OverlapDist")
+RS<- llply(cov.mx[mask.n.size], RandomSkewers, num.vectors = 1000, repeat.vector = )
 mask2 <- ldply(stats, function(x) !is.na(x[1]))[,2]
 stats <- stats[mask1]
 
