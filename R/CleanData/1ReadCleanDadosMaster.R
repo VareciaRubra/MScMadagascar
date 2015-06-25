f9853757bfc07f9ad8db110e70bf2406bef270da
Anna Penna @Darwin <annapontopenna@gmail.com>
2015-06-25 07:19:24 +0400

    Read and ordered complete database following J.Hrrera & L.Davalos phylogeny.

--- R/CleanData/1ReadCleanDadosMaster.R
+++ R/CleanData/1ReadCleanDadosMaster.R
@@ -17,22 +17,51 @@ registerDoParallel(cores = 15)
 #abrir no terminal htop para ver os cores trabalhando
 
 arquivo.bruto = "Data/1Master_Organized_Factors.csv"
-arquivo.saida =  "Data/1Master_Organized_Factors.csv"
+arquivo.saida =  "Data/1MasterDB_following_JHerrera_PhylOrder.csv"
 #read csv and create table dataframe
 raw.regular <- read.csv(arquivo.bruto, head = T)
-raw.data<- tbl_df(raw.regular)
-raw.data$Especie %<>% gsub("\\.", "", .)
-# forçando a ordem dos fatores  como sendo a ordem que aparece 
-# no proprio dataframe-------> 
-#isso evita que funçoes da classe apply reoordenem os resultados numérica/alfabeticamente.
+raw.regular$Especie %<>% gsub("\\.", "", .)
+########## Renomeando minhas especies de acordo com a arvore do James ############
+recoderFunc <- function(data, oldvalue, newvalue) {
+  # convert any factors to characters
+  if (is.factor(data))     data     <- as.character(data)
+  if (is.factor(oldvalue)) oldvalue <- as.character(oldvalue)
+  if (is.factor(newvalue)) newvalue <- as.character(newvalue)
+  
+  # create the return vector
+  data[data == 0] <- NA
+  newvec <- data
+  # put recoded values into the correct position in the return vector
+  for (i in unique(oldvalue)) newvec[data == i ] <- newvalue[oldvalue == i ]
+  newvec
+}
+rename.james<- read.csv("Data/myspecies.csv", head = T)
+raw.regular$Especie<- recoderFunc(data = raw.regular$Especie, oldvalue = rename.james$Anna, newvalue = rename.james$James)
 
-raw.data$Tombo <- factor (raw.data$Tombo, levels = unique(raw.data$Tombo) )
-
+################################ Colocando a ordem dos fatores de espécie como sendo a ordem em que eles aparecem na árvore
 ################## Árvore James ##################
 treefile = read.nexus(file = "~/ataches/fbd369agerange_gooddates.tre")
-species <- treefile$tip.label[treefile$tip.label %in% unique(raw.data$Especie)]
+species <- treefile$tip.label[treefile$tip.label %in% unique(raw.regular$Especie)]
+################# Arvore indexada pelas especies que eu tenho na amostra #############
 pruned.tree<-drop.tip(treefile,treefile$tip.label[-match(species, treefile$tip.label)])
 plot(pruned.tree)
+tiporder<- treefile$edge[,2][ treefile$edge[, 2] %in% 1:length(treefile$tip.label) ]
+ordered.species <- as.data.frame(as.character(treefile$tip.label[tiporder]) )
+names(ordered.species)<- ".id"
+my.species <- unique(raw.regular$Especie)
+species <- ordered.species[ordered.species$.id %in% my.species,]
+raw.regular$Especie <- factor (raw.regular$Especie, levels = species )
+######### salvando no formato tbl_df
+raw.data<- tbl_df(raw.regular)
+raw.data %<>% arrange(., raw.data$Especie )
+# forçando a ordem dos fatores  como sendo a ordem que aparece 
+# no proprio dataframe------->  ordenado de acordo com a arvore do James ################
+#isso evita que funçoes da classe apply reoordenem os resultados numérica/alfabeticamente.
+raw.data$Especie <- factor (raw.data$Especie, levels = unique(raw.data$Especie) )
+raw.data$Genero <- factor (raw.data$Genero, levels = unique(raw.data$Genero) )
+raw.data$Familia <- factor (raw.data$Familia, levels = unique(raw.data$Familia) )
+raw.data$Tombo <- factor (raw.data$Tombo, levels = unique(raw.data$Tombo) )
+
 raw.data %<>% 
   filter(Tombo != "044001b") %>% #comentario de pq foi removido
   filter(Tombo != "2386(16b)") %>%
@@ -42,8 +71,8 @@ raw.data %<>%
   
 
 #contando quantos tem por espécie e por Take (pra ter certeza de que se removeu um cara sairam as duas replicas)
-raw.data %>% count(Especie) 
-raw.data %>% count(Take)
+raw.data %>% dplyr::count(Especie) 
+raw.data %>% dplyr::count(Take)
 #write.csv(raw.data, arquivo.saida , row.names= F)
 
 #organizando databases por ordem taxonomica 
@@ -54,5 +83,5 @@ Gen.raw.main.data<- dlply(raw.data, .(Genero), tbl_df)
 Sp.raw.main.data<- dlply(raw.data, .(Especie), tbl_df)
 
 
-#write.csv(raw.data, arquivo.saida , row.names= F)
+write.csv(raw.data, arquivo.saida , row.names= F)
 
