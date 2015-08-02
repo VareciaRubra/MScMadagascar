9e66e99413a557f3066268d3590bc6a705455881
Anna Penna @Darwin <annapontopenna@gmail.com>
2015-08-03 02:02:51 +0400

    change minimum nsize to built matrix to 15

--- R/Limpeza/3MakeMainData.R
+++ R/Limpeza/3MakeMainData.R
@@ -27,10 +27,10 @@ makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, fin
     sp.fit <-  if (specie == FALSE & sp.number > 1 & sum((table(x[[3]]$Especie)>=3) == FALSE) ==0  ) manova(as.matrix(x[[4]]) ~ Especie, data = as.data.frame(x[[3]])) else NA
     gr <- if(specie == TRUE | specie == FALSE & sp.number <= 1 | specie == FALSE & sum((table(x[[3]]$Especie)>=3) == FALSE) !=0    ) var(na.omit(x[[4]])) else CalculateMatrix(sp.fit) 
     pq <- matrix(data = NA, nrow = 39, ncol = 39) 
-  x[[11]][[1]] <- if(x[[9]]>15) gr else pq
-  x[[11]][[2]] <- if(x[[9]]>15) cov2cor(x[[11]][[1]]) else pq
-  x[[11]][[3]] <- if(x[[9]]>15) var(na.omit(x[[5]]) ) else pq
-  x[[11]][[4]] <- if(x[[9]]>15) var(na.omit(x[[6]]) ) else pq
+  x[[11]][[1]] <- if(x[[9]]>14) gr else pq
+  x[[11]][[2]] <- if(x[[9]]>14) cov2cor(x[[11]][[1]]) else pq
+  x[[11]][[3]] <- if(x[[9]]>14) var(na.omit(x[[5]]) ) else pq
+  x[[11]][[4]] <- if(x[[9]]>14) var(na.omit(x[[6]]) ) else pq
   x[[11]][[5]] <- sp.fit 
   x[[11]][[6]] <- if( !is.na(x[[11]][[1]][1]) ) ExtendMatrix(cov.matrix = x[[11]][[1]], var.cut.off = 1e-04, ret.dim = NULL) else pq
   names(x)[11] <- 'matrix'
@@ -40,11 +40,11 @@ makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, fin
   names(x[[13]])[1:4] <-  c('cov','cor', 'cov.sizeless', 'cov.log')
   if (compare.size == TRUE)
   {x[[12]] <- if(x[[9]]>40) ldply(.data = x[[11]][-c(2,5,6)], .fun = MeanMatrixStatistics, parallel = TRUE, full.results = FALSE) else NA
-   x[[13]][1]<- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
-   x[[13]][2]<- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
-   x[[13]][3]<- if(x[[9]]>15) BootstrapRep( x[[5]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
-   x[[13]][4]<- if(x[[9]]>15) BootstrapRep( x[[6]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
-   x[[14]] <- if(x[[9]]>15) RandomSkewers(cov.x = x[[11]][c(1,3,4)], num.vectors = 1000, repeat.vector = unlist(x[[13]])[c(1,3,4)]  ) else NA
+   x[[13]][1]<- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
+   x[[13]][2]<- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+   x[[13]][3]<- if(x[[9]]>14) BootstrapRep( x[[5]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
+   x[[13]][4]<- if(x[[9]]>14) BootstrapRep( x[[6]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = TRUE) else NA
+   x[[14]] <- if(x[[9]]>14) RandomSkewers(cov.x = x[[11]][c(1,3,4)], num.vectors = 1000, repeat.vector = unlist(x[[13]])[c(1,3,4)]  ) else NA
   }   
   names (x)[12:14] <- c('mean.mx.stats', 'size.mx.rep.rs', 'rs.size.comparisson')
   
@@ -54,21 +54,21 @@ makeMainData <- function (current.data, specie = TRUE, compare.size = FALSE, fin
   x[[17]] <- vector ("list", 3)
   if (final == TRUE)
   {
-    x[[15]][[1]] <- if(x[[9]]>15) Rarefaction(x[[4]], RandomSkewers, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-    x[[15]][[2]] <- if(x[[9]]>15) Rarefaction(x[[4]], KrzCor, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-    x[[15]][[3]] <- if(x[[9]]>15) Rarefaction(x[[4]], PCAsimilarity, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-    x[[15]][[4]] <- if(x[[9]]>15) Rarefaction(x[[4]], MatrixCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
-    x[[15]][[5]] <- if(x[[9]]>15) Rarefaction(x[[4]], KrzCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[1]] <- if(x[[9]]>14) Rarefaction(x[[4]], RandomSkewers, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[2]] <- if(x[[9]]>14) Rarefaction(x[[4]], KrzCor, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[3]] <- if(x[[9]]>14) Rarefaction(x[[4]], PCAsimilarity, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[4]] <- if(x[[9]]>14) Rarefaction(x[[4]], MatrixCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
+    x[[15]][[5]] <- if(x[[9]]>14) Rarefaction(x[[4]], KrzCor, correlation = TRUE, iterations = 1000, num.reps = x[[9]], parallel = T) else NA
         
-    x[[16]][[1]][1] <- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
-    x[[16]][[1]][2] <- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, parallel = T) else NA
-    x[[16]][[1]][3] <- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = PCAsimilarity, iterations = 1000, parallel = T) else NA
-    x[[16]][[1]][4] <- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
-    x[[16]][[1]][5] <- if(x[[9]]>15) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+    x[[16]][[1]][1] <- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = RandomSkewers, iterations = 1000, parallel = T) else NA
+    x[[16]][[1]][2] <- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, parallel = T) else NA
+    x[[16]][[1]][3] <- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = PCAsimilarity, iterations = 1000, parallel = T) else NA
+    x[[16]][[1]][4] <- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = MantelCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
+    x[[16]][[1]][5] <- if(x[[9]]>14) BootstrapRep( x[[4]], ComparisonFunc = KrzCor, iterations = 1000, correlation = TRUE, parallel = T) else NA
     
-    x[[17]][[1]] <- if(x[[9]]>15) BootstrapR2(ind.data= x[[4]], iterations = 1000, parallel = TRUE) else NA
-    x[[17]][[2]] <- if(x[[9]]>15) BootstrapR2(ind.data= x[[5]], iterations = 1000, parallel = TRUE) else NA
-    x[[17]][[3]] <- if(x[[9]]>15) BootstrapR2(ind.data= x[[6]], iterations = 1000, parallel = TRUE) else NA
+    x[[17]][[1]] <- if(x[[9]]>14) BootstrapR2(ind.data= x[[4]], iterations = 1000, parallel = TRUE) else NA
+    x[[17]][[2]] <- if(x[[9]]>14) BootstrapR2(ind.data= x[[5]], iterations = 1000, parallel = TRUE) else NA
+    x[[17]][[3]] <- if(x[[9]]>14) BootstrapR2(ind.data= x[[6]], iterations = 1000, parallel = TRUE) else NA
     
   }  
   
@@ -128,7 +128,7 @@ gen.master.main.data <- llply(Gen.raw.main.data, specie = FALSE, final = FALSE, 
 ############# TERCEIRA PARTE ################
 ## Colocando Mx bem estimadas para os n<27###
 #############################################
-#registerDoParallel(cores = 20)
+registerDoParallel(cores = 20)
 sp.main.data <- llply(Sp.raw.main.data, specie = TRUE, compare.size = TRUE, final = TRUE, makeMainData, .progress = progress_text(char = "."), .inform = T)
 gen.main.data <- llply(Gen.raw.main.data, specie = FALSE, compare.size = TRUE, final = TRUE, makeMainData, .progress = progress_text(char = "."), .inform = T)
   
