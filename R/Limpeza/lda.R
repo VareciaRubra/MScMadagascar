6213e3c3dcccd460f0593ae8a50cce6d4918ece3
Anna Penna @Darwin <annapontopenna@gmail.com>
2015-08-10 09:42:37 +0400

    discriminant analisis stuff arrumar para o novo formato de dados

---
+++ R/Limpeza/lda.R
@@ -0,0 +1,36 @@
+library(MASS)
+
+ordered<- info[info$Tombo %in% dimnames(MEAN.ind)[[1]], ]
+
+restricted.info<- ordered[ordered$Especie %in% c("Avahi laniger","Avahi peyrierasi "), ]
+restricted.ed<- MEAN.ind[rownames(MEAN.ind) %in% restricted.info$Tombo,]
+table(ordered$Especie)
+table(restricted.info$Especie)
+
+
+lfunction<-lda(restricted.ed, restricted.info$Especie)
+predicted.scores  <- predict(lfunction, restricted.ed)
+
+write.csv2(coef(lfunction), file="coef_lda.csv")
+
+source('confusion.R')
+
+cross.v<-lda(restricted.ed, restricted.info$Especie, CV=TRUE)
+confu<-confusion(restricted.info$Especie, predicted.scores$class, printit=FALSE)
+confu.CV<-confusion(restricted.info$Especie, cross.v$class, printit=FALSE)
+
+ordered$Especie <- as.factor(ordered$Especie)
+restricted.info$Especie<- as.factor(restricted.info$Especie)
+
+confusion.ld<-matrix(0,length(levels(restricted.info$Especie)),length(levels(restricted.info$Especie)))
+rownames(confusion.ld)<-levels(restricted.info$Especie)
+colnames(confusion.ld)<-levels(restricted.info$Especie)
+
+nsp<-length(levels(restricted.info$Especie))
+for(i in 1:nsp) for(j in 1:nsp){
+  confusion.ld[i,j]<-paste(round(confu[i,j],2),"(", round(confu.CV[i,j],2),")", sep="")
+}
+
+write.csv2(confusion.ld, file="confusion.csv")
+
+confusion
