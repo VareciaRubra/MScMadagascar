cc847945ffcac24c1a08f21391e65d6cbc0fadf1
Anna Penna <annapontopenna@gmail.com>
2015-12-11 22:10:01 -0200

    vendo o efeito de filogenia nos valores de comparação de matrizes

--- R/Chapter 1/MX compare.R
+++ R/Chapter 1/MX compare.R
@@ -69,6 +69,19 @@
   range.values<- range(mat_data, na.rm = T)
   diag(mat_data) <- sample.size.list
   
+  ############################# Vendo efeito de filogenia ##########################################
+  notat.tree<- is.na(match(dimnames(mx.compare$RS$correlations)[[1]], treefile$tip.label)) 
+  names.at.tree <- dimnames(mx.compare$RS$correlations)[[1]][!notat.tree] 
+  mx.all.at.tree<- mx.compare$RS$correlations[!notat.tree,!notat.tree]
+  pruned.tree.all<- drop.tip(treefile,treefile$tip.label[-match( names.at.tree, treefile$tip.label)])
+  phylo.dist.all.at.tree<- cophenetic.phylo(pruned.tree.all)
+  phylo.dist.all.at.tree<- phylo.dist.all.at.tree[rownames( mx.all.at.tree), rownames( mx.all.at.tree)]
+  MatrixCor(phylo.dist.all.at.tree, mx.all.at.tree)
+  ##################################################################################################
+  
+  ################## Ponderar os valores de comparação pelo tamanho amostral : média armonica
+  
+  ##################################################################################################
   Combine.Mx.Plot <- function(Mx1, Mx2, diag.info = NA, titulo = "blé"){
     
     mat_data <- Mx1
