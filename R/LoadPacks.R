6307cd91709a8ffac5b0f9c81ca2393155f6b23b
Anna Penna <annapontopenna@gmail.com>
2016-02-22 19:15:16 -0300

    mvn stuff

--- R/LoadPacks.R
+++ R/LoadPacks.R
@@ -4,7 +4,6 @@ if(!require(car)) {install.packages('car'); library(car)}
 if(!require(magrittr)) {install.packages('magrittr'); library(magrittr)}
 if(!require(lme4)) {install.packages('lme4'); library(lme4)}
 if(!require(ggplot2)) {install.packages('ggplot2'); library(ggplot2)}
-if(!require(phylobase)) {install.packages('cowplot'); library(phylobase)}
 if(!require(tidyr)) {install.packages('tidyr'); library(tidyr)}
 if(!require(MCMCglmm)) {install.packages('MCMCglmm'); library(MCMCglmm)}
 if(!require(reshape2)) {install.packages('reshape2'); library(reshape2)}
@@ -15,6 +14,9 @@ if(!require(doParallel)) {install.packages('doParallel'); library(doParallel)}
 if(!require(phylobase)) {install.packages('phylobase'); library(phylobase)}
 if(!require(RColorBrewer)) {install.packages('RColorBrewer'); library(RColorBrewer)}
 if(!require(cowplot)) {install.packages('cowplot'); library(cowplot)}
+if(!require(mvoutlier)) {install.packages('mvoutlier'); library(mvoutlier)}
+if(!require(MVN)) {install.packages('MVN'); library(MVN)}
+
 #if(!require()) {install.packages(''); library()}
 
 ################### Update do R e mais um monte de zica de pacotes e dependencias
