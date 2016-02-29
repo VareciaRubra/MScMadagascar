3c43d20fa9cc23a5c4671aa4d7081a308ce2b573
Anna Penna <anna.penna@usp.com>
2016-02-29 10:42:04 -0300

    plots comparativos

--- R/Chapter 1/MxRarefaction.R
+++ R/Chapter 1/MxRarefaction.R
@@ -1,3 +1,8 @@
+############# Rarefação no calculo das matrizes 
+# pegando as maiores amostras de duas espécies nos extremos da variaçao de tamanho : Microcebus griseorrufus e Indri indri
+# por dois métodos de comparação RS e KRZ 
+# tudo ta calculado já no sp.main.data
+
 mean.sim<-function(x) {
     x[lower.tri(x)]<-t(x)[lower.tri(x)]
   diag(x)<-NA
@@ -46,12 +51,14 @@ ic.min.sim<-function(x) {
   return(ic)
 }
 
-mean.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[-c(43:44), -c(43:44)] ) %>% ldply (function (x) mean.sim(x) )
-min.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[-c(43:44), -c(43:44)] ) %>% ldply (function (x) min.sim(x) )
-max.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[-c(43:44), -c(43:44)] ) %>% ldply (function (x) max.sim(x) )
-ic.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[-c(43:44), -c(43:44)] ) %>% ldply (function (x) ic.sim(x) )
-ic.min.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[-c(43:44), -c(43:44)] ) %>% ldply (function (x) ic.min.sim(x) )
-ic.max.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[-c(43:44), -c(43:44)] ) %>% ldply (function (x) ic.max.sim(x) )
+### Add os ranges e valores médios de comparação por espécie no grafico de rarefaçao
+
+mean.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[c(3:30,32:34), c(3:30,32:34)] ) %>% ldply (function (x) mean.sim(x) )
+min.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[c(3:30,32:34), c(3:30,32:34)] ) %>% ldply (function (x) min.sim(x) )
+max.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[c(3:30,32:34), c(3:30,32:34)] ) %>% ldply (function (x) max.sim(x) )
+ic.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[c(3:30,32:34), c(3:30,32:34)] ) %>% ldply (function (x) ic.sim(x) )
+ic.min.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[c(3:30,32:34), c(3:30,32:34)] )  %>% ldply (function (x) ic.min.sim(x) )
+ic.max.compare.cov <- mx.compare[c(1:2)] %>% llply (function (x) x$correlations[c(3:30,32:34), c(3:30,32:34)] ) %>% ldply (function (x) ic.max.sim(x) )
 
 dimnames(mean.compare.cov)[[1]] <- c(paste("Mean", mean.compare.cov[,1], sep = "."))
 dimnames(min.compare.cov)[[1]] <- c(paste("Min", min.compare.cov[,1], sep = "."))
@@ -61,70 +68,66 @@ dimnames(ic.compare.cov)[[1]][c(2,4)] <- c(paste("ic.max", ic.compare.cov[,1][c(
 dimnames(ic.max.compare.cov)[[1]] <- c(paste("Max", ic.max.compare.cov[,1], sep = "."))
 dimnames(ic.min.compare.cov)[[1]] <- c(paste("Min", ic.min.compare.cov[,1], sep = "."))
 
-range.compare <- rbind(mean.compare.cov, ic.min.compare.cov, ic.max.compare.cov, ic.compare.cov)
-range.compare <- range.compare[,-1]
-range.compare <- as.data.frame(t(range.compare))
+range.compare.large <- rbind(mean.compare.cov, ic.min.compare.cov, ic.max.compare.cov, ic.compare.cov)
+range.compare.large <- range.compare.large[,-1]
+range.compare.large <- as.data.frame(t(range.compare.large))
 
-sample.size.list <- n.size[mask,2]
-range.compare$n <- sample.size.list
-range.compare$.id <- rownames(range.compare)
 
-############# Rarefação no calculo das matrizes 
-# pegando as maiores amostras de duas espécies nos extremos da variaçao de tamanho : Microcebus griseorrufus e Indri indri
-# por dois métodos de comparação RS e KRZ 
-# tudo ta calculado já no sp.main.data
+sample.size.list <- n.size[mask,2][c(3:30,32:34)]
+range.compare.large$n <- sample.size.list
+range.compare.large$.id <- rownames(range.compare.large)
 
+str(range.compare.large)
+
+#range.compare.backup <- range.compare
+range.compare <- range.compare.large
+range.compare <- range.compare.backup
 #sp.main.data$Microcebus_griseorufus$rarefaction$rs
   plot.df <- melt(temp.rar.rs , value.name = "Mean.BS.RS")
   names(plot.df) <- c("Mean.BS.RS", "n") 
   plot.df <- as.data.frame(lapply(plot.df, as.numeric))
-rarefaction.plot.rs <- 
+rarefaction.plot.rs<- 
     ggplot(data = plot.df, aes(x= n, y = Mean.BS.RS)) + 
     geom_boxplot(aes( group = n), color="grey", outlier.shape = 8 ) + 
-    scale_x_continuous(x.axis) + 
     geom_point(data = range.compare, aes(x = n, y = Mean.BS.RS), shape=17, size = 2.2, color = "red") +
-    #geom_text(data = range.compare, aes(x = n, y = Mean.BS.RS, label = .id), color = "red", size = 3, alpha = 0.3, angle = 70, hjust =  0.6) +
+    #geom_text(data = range.compare, aes(x = n, y = Mean.BS.RS, label = .id), color = "blue", size = 3, alpha = 0.3, angle = 70, hjust =  0.6) +
     geom_errorbar(data = range.compare, aes(x= n, ymin = Min.BS.RS, ymax = Max.BS.RS), alpha = 0.3, size = 1.2, color = "red") +
-    geom_hline(data = range.compare, aes(yintercept = mean (Mean.BS.RS), y = mean (Mean.BS.RS)), alpha = 0.1, size = 2.5) +
-    ggtitle("Rarefaction and Observed range of similarity") +
+    geom_hline(data = range.compare, aes(yintercept = mean (Mean.BS.RS), y = mean (Mean.BS.RS)), alpha = 0.1, size = 2.5, color = "red") +
+  #  ggtitle("Rarefaction and Observed range of similarity") +
     theme(plot.title = element_text(lineheight=.8, face="bold"),
             axis.text.x = element_text(size = 15), 
             axis.title.x = element_text(size = 15)) +
-    xlab("Random Skewers") + ylab("Number of sampled specimens") + 
-    scale_y_continuous(y.axis, limits = c(0,1), breaks = c(0.00, 0.25, 0.5, 0.66, 0.75, 1.00)) +
-    scale_x_continuous(x.axis, limits = c(0, 68)) +
+    ylab(label = "Random Skewers") + 
+  xlab("") +
+   #xlab("Number of sampled specimens") + 
+    scale_y_continuous(limits = c(0,1), breaks = c(0.00, 0.25, 0.5, 0.66, 0.75, 1.00)) +
+    scale_x_continuous(limits = c(0, 68)) +
     theme_bw()
     
 
     plot.df <- melt(temp.rar.krz, value.name = "Mean.BS.KRZ")
     names(plot.df) <- c("Mean.BS.KRZ", "n") 
     plot.df <- as.data.frame(lapply(plot.df, as.numeric))
-rarefaction.plot.krz <- 
+rarefaction.plot.krz<- 
     ggplot(data = plot.df, aes(x= n, y = Mean.BS.KRZ)) + 
       geom_boxplot(aes( group = n), color="grey", outlier.colour = "grey", outlier.shape = 8) + 
-      scale_x_continuous(x.axis, name = "Number of sampled specimens") + 
       geom_point(data = range.compare, aes(x = n, y = Mean.BS.KRZ), shape=17, size = 2.2, color = "red") +
-      #geom_text(data = range.compare, aes(x = n, y = Mean.BS.KRZ, label = .id), color = "red", size = 3, alpha = 0.3, angle = 70, hjust =  0.6) +
+      #geom_text(data = range.compare, aes(x = n, y = Mean.BS.KRZ, label = .id), color = "blue", size = 3, alpha = 0.3, angle = 70, hjust =  0.6) +
       geom_errorbar(data = range.compare, aes(x= n, ymin = Min.BS.KRZ, ymax = Max.BS.KRZ), alpha = 0.3, size = 1.2, color = "red") +
-      geom_hline(data = range.compare, aes(yintercept = mean (Mean.BS.KRZ), y = mean (Mean.BS.KRZ)), alpha = 0.1, size = 2.5) +
-      ggtitle("Rarefaction and Observed range of similarity") +
+      geom_hline(data = range.compare, aes(yintercept = mean (Mean.BS.KRZ), y = mean (Mean.BS.KRZ)), alpha = 0.1, size = 2.5, color = "red") +
+   #   ggtitle("Rarefaction and Observed range of similarity") +
       theme(plot.title = element_text(lineheight=.8, face="bold"),
             axis.text.x = element_text(size = 15), 
             axis.title.x = element_text(size = 15)) +
-  xlab("Krzanowski") + ylab("Number of sampled specimens") + 
-      scale_y_continuous(y.axis, limits = c(0,1), breaks = c(0.00, 0.25, 0.5, 0.75, 0.82, 1.00)) +
-      scale_x_continuous(x.axis, limits = c(0, 68)) +
+      ylab("Krzanowski") + 
+  #  xlab("") +
+  xlab("Number of sampled specimens") + 
+      scale_y_continuous(limits = c(0,1), breaks = c(0.00, 0.25, 0.5, 0.75, 0.82, 1.00)) +
+      scale_x_continuous(limits = c(0, 68)) +
       theme_bw()
+micro.rar.obs.sim <- plot_grid(rarefaction.plot.rs, rarefaction.plot.rs.large, rarefaction.plot.krz, rarefaction.plot.krz.large, labels = LETTERS[1:4], ncol=2)
 
-
-micro.rar.obs.sim <- plot_grid(rarefaction.plot.rs, rarefaction.plot.krz,labels = LETTERS[1:2])
-
-
-
-
-
-### Add os ranges e valores médios de comparação por espécie no grafico de rarefaçao
-
+#Antigo plot de rarefaçao
 p1 <- PlotRarefaction(sp.main.data$Microcebus_griseorufus$rarefaction$krz) + 
   labs(title = "Microcebus griseorufus") + 
   theme(plot.title = element_text(face = "italic", size = 20), 
@@ -136,38 +139,3 @@ p1 <- PlotRarefaction(sp.main.data$Microcebus_griseorufus$rarefaction$krz) +
   geom_point(data = range.compare, aes(x = L1, y = Mean.BS.KRZ)) +
   coord_cartesian(ylim=c(0.4, 1)) + 
   scale_y_continuous("KRZ", breaks=seq(0.4, 1, 0.2))
-
-
-p2 <- PlotRarefaction(sp.main.data$Indri_indri$rarefaction$krz) + 
-  labs(title = "Indri indri") + 
-  theme(plot.title = element_text(face = "italic", size = 20), 
-        axis.title = element_blank(), 
-        axis.text =element_blank(), 
-        axis.ticks = element_line(size =0)) +
-  coord_cartesian(ylim=c(0.4, 1)) + 
-  scale_y_continuous( breaks=seq(0.40, 1, 0.2))
-
-p3 <- PlotRarefaction(sp.main.data$Microcebus_griseorufus$rarefaction$rs) + 
-  theme(axis.text= element_text(size = 15)) +
-  coord_cartesian(ylim=c(0, 1)) + 
-  scale_y_continuous("RS", breaks=seq(0.25, 1, 0.25))
-
-p4 <- PlotRarefaction(sp.main.data$Indri_indri$rarefaction$rs) + 
-  theme(axis.text= element_text(size = 15), 
-        axis.title.y = element_blank(), 
-        axis.text.y =element_blank(), 
-        axis.ticks.y = element_line(size =0)) +
-  coord_cartesian(ylim=c(0, 1)) + 
-  scale_y_continuous(breaks=seq(0.25, 1, 0.25))
-
-plot_grid(p1, p2, p3, p4)  
-
-
-
-
-
-
-  
-  
-
-
