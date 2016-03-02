5f3cc9fa5fbdb4f003b0936145144e74298c8d6e
Anna Penna <annapontopenna@gmail.com>
2016-03-02 20:52:14 -0300

    verificações

--- PartyVariation.R
+++ PartyVariation.R
@@ -5,13 +5,18 @@
 #1# Verificar se as matrizes de resultados de comparação sao euclidianas: ade4::is.euclid()
 mx.compare %>% llply(function(x) x$correlations) %>% llply(., .fun = dist) %>% llply(., .fun = is.euclid)
 Steppan[1:2] %>% llply(., .fun = dist) %>% llply(., .fun = is.euclid)
-
 #ok, todas é!
+# vendo se as matrizes sao simetricas
+mx.compare %>% llply(function(x) x$correlations) %>%  llply(., .fun = isSymmetric ) 
+### a matriz nao é simetrica pois sao os outputs das funções de comparação do pacote.
+#mas o defaut da função dist é pegar a lower tri (onde tao os valores nao corrigidos das comparações, entao ta tudo bem)
+
 
 #2# Transformar os valores de similaridade de matrizes em valores de diferença usando sqrt(1-semelhança)
 mx.dissimilarity <- mx.compare %>% llply(function(x) x$correlations) %>% llply( function (x) sqrt(1-x) )
 mx.dissimilarity$Steppan.RS <- sqrt(abs(1-Steppan$RS) )
 mx.dissimilarity$Steppan.KRZ <- sqrt(abs(1-Steppan$KRZ) )
+
 names(mx.dissimilarity)
 dimnames(mx.dissimilarity$BS.RS)
 
@@ -45,8 +50,21 @@ mask <- ldply(cov.mx, function(x) !is.na(x[1]))[,2]
 multi.means<- multi.means[mask]
 
 MahalaLemur<- MultiMahalanobis(means =multi.means, cov.matrix = W.matrix)
+MahalaLemur %>%  dist %>% is.euclid
 
 #6# montando as PCOA de tudo essas coisa ae: ape:: pcoa
 mx.pcoa <- mx.dissimilarity %>% llply(., .fun = dist) %>% llply(., rn = dimnames(mx.all.at.tree)[[1]], .fun = pcoa, .progress = "text") 
+mx.pcoa$BS.RS$values
+mx.pcoa$Steppan.RS$values
+mx.pcoa$BS.KRZ$values
+
 phylo.pcoa <- phylo.dist %>% dist %>% pcoa
+phylo.pcoa$values
 mahala.pcoa <- MahalaLemur %>% dist %>% pcoa
+mahala.pcoa$values
+# tentando jogar essas coisas no vegan
+varpart(mx.pcoa$BS.RS$vectors[1:10], phylo.pcoa$vectors[1:10], mahala.pcoa$vectors[1:10]  )
+rda(mx.pcoa$BS.RS$vectors[1:10], phylo.pcoa$vectors[1:10])
+rda(mx.pcoa$BS.RS$vectors[1:10], mahala.pcoa$vectors[1:10])
+
+
