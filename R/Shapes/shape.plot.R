444aee587fd0d98f32a849133724eb0dbebdc5b2
Anna Penna <anna.penna@usp.com>
2016-03-14 03:06:23 -0300

    shapes!

--- R/Shapes/shape.plot.R
+++ R/Shapes/shape.plot.R
@@ -126,18 +126,24 @@ hapa.sym <- hapa.sym [which (rownames (hapa.sym) %in% rownames (Reference)), ]
 
 hapa.sym <- hapa.sym [match(rownames (Reference), rownames (hapa.sym)), ]
 
-shape.plot <- list()
 
-for (i in 1:4)
+
+myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")), space="Lab")
+shape.plot <- list()
+for (i in 1:39)
   shape.plot [[i]] <-
   ggshape(hapa.sym,
           Aux $ tessel.39 [1:39, ],
           WPCs[, i],
           rotation = c(1, -1, 1), culo = 0.01, thickness = 2) +
-  ggtitle(i)
-
-shape.plot [[5]] <- plot_grid(plotlist = shape.plot, ncol = 2)
-
-shape.plot [[5]]
+ ggtitle(paste("PC", i, sep = " ")) +
+  theme(plot.title = element_text(face = "bold", size = 12),
+        legend.position= c(0.3,0.1), legend.direction="horizontal") 
+  shape.plot [[1]]
+rm(i)
+shape.plot [[40]] <- plot_grid(plotlist = shape.plot, ncol = 4) + ggtitle("Trais scores on W\nusing Hapalemur frame")
+shape.plot [[41]] <- plot_grid(shape.plot[[1]], shape.plot[[2]],shape.plot[[3]], shape.plot[[4]], ncol = 1) + ggtitle("Trais scores on W\nusing Hapalemur frame")
+shape.plot [[41]]
+rm(shape.plot)
 
 save (shape.plot, file = 'shape.plot.RData')
