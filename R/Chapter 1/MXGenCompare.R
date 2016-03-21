eb1505d79e1db582362767605f42e9839c9deb07
Anna Penna <annapontopenna@gmail.com>
2016-03-21 19:42:17 -0300

    ble

--- R/Chapter 1/MXGenCompare.R
+++ R/Chapter 1/MXGenCompare.R
@@ -127,5 +127,3 @@ plot(pruned.tree.with.mx, cex = 0.9, no.margin =T, edge.color = "darkgrey", edge
 co <- c("red", "blue")
 tiplabels(pch = 22, bg = co[as.numeric(gm.means[,2])], cex = 2, adj = 1)
 nodelabels(thermo = ace.gm$lik.anc, piecol = co, cex = 0.75)
-
-fastAnc(pruned.tree.with.mx,gm.means[,2], CI= T) 
