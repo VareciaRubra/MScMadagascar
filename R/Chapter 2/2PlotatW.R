6fe16e746496652741c72de01a9d4e144476f300
Anna Penna <annapontopenna@gmail.com>
2016-03-23 16:31:40 -0300

    change directory

---
+++ R/Chapter 2/2PlotatW.R
@@ -0,0 +1,197 @@
+#Entrando a árvore
+#Arvore
+treefile = read.nexus(file = "attaches/fbd421agerange_edited.tre")
+species <- treefile$tip.label[treefile$tip.label %in% names(sample.no.na)]
+pruned.tree<-drop.tip(treefile,treefile$tip.label[-match(species, treefile$tip.label)])
+plot(pruned.tree)
+nodelabels()
+#matrizes ancestrais ponderadas pela filogenia para cada nó da arvore: 
+#fazer a chamada pelo numero correspondente do nó na filogenia
+Ancestral.Matrices<- PhyloW(tree = pruned.tree, tip.data = cov.no.na, tip.sample.size = sample.no.na)
+#matriz W selecionada a partir da PhyloW do pacote ATENÇÃO QUE A GRAFIA TEM QUE SER COM ASPAS PARA NUMERO DO NÓ
+Wmat <- Ancestral.Matrices$"42" 
+#all.main.data$All$info [aaply (as.matrix (all.main.data$All$ed), 1, function (L) any (is.na(L))), ]
+GeralMorphoSpace = CalculateMatrix(manova(as.matrix(all.main.data$All$ed) ~ Especie, data = as.data.frame(all.main.data$All$info) ))
+Wmat <- GeralMorphoSpace
+#definindo qual o conjunto de dados que quero plotar no espaçõ da matriz determinada:
+current.data <- all.main.data #todo o banco de dados
+current.data <- extant.main.data #apenas os viventes
+# current.data<- madagascar.main.data # apenas os de madagascar
+# current.data <- extant.madagascar.main.data # apenas os viventes de madagascar
+
+current.data$All$info$Familia <- factor(current.data$All$info$Familia, levels = unique(current.data$All$info$Familia))
+
+#juntando as informações de cada indivíduo com suas respectivas distancias
+lin_data = ldply(current.data, function(x) tbl_df(cbind(x$info, x$ed))) 
+#Complete info
+lin_data %<>% mutate (., .Cinfo = paste(Status, Regiao, Familia, Genero, Especie, sep = ".")) #criando uma coluna de info que vai ter dados taxonomicos de familia e genero (para depois separar o plot em cores)
+#Resumed info
+lin_data %<>% mutate (., .Rinfo = paste(Status, Regiao, Familia, Genero, sep = ".")) #criando uma coluna de info que vai ter dados taxonomicos de familia e genero (para depois separar o plot em cores)
+#myformula = paste0('~', paste(names(select(lin_data, IS_PM:BA_OPI)), collapse = '+'))
+lin_data$Tombo <- factor(x = lin_data$Tombo, levels = unique(lin_data$Tombo))
+rownames(lin_data) <- lin_data$Tombo
+
+######## Biplot dos scores de cada indivíduo no espaço da W desejada##############
+lin_data <- select(lin_data, .Cinfo, .Rinfo, Tombo, Especie, IS_PM:BA_OPI)
+mx.all <- as.matrix(na.omit(select(current.data$All$ed, IS_PM:BA_OPI) ) )
+mx.all<- scale(mx.all, center = TRUE, scale = FALSE)
+resp <- as.data.frame(mx.all %*% eigen(Wmat)$vectors[,c(1:39)])
+resp %<>% mutate(., .id = rownames(resp) )
+names(resp) %<>% gsub("V", "PC", .)
+resp.info <- select(lin_data, c(.Cinfo, .Rinfo, Tombo) )
+resp.info <- resp.info[resp.info$Tombo %in% resp$.id, ]
+table(as.character(resp.info$Tombo) == resp$.id)
+plot.W <- cbind(resp.info, resp)
+plot.W$PC1 <- plot.W$PC1 *(-1) + 90
+plot.W$PC2 <- plot.W$PC2 *(-1) 
+plot.W$PC4 <- plot.W$PC4 *(-1) + 30
+
+  hulls <- ddply(plot.W, .(.Cinfo), plyr::summarise, "hPC1"=PC1[chull(PC1,PC4)],
+                 "hPC4"=PC4[chull(PC1,PC4)], "tombo"=Tombo[chull(PC1,PC4)])
+  
+  hulls <- ddply(plot.W, .(.Cinfo), plyr::summarise, "hPC1"=PC1[chull(PC1,PC4)],
+                 "hPC4"=PC4[chull(PC1,PC4)])
+  hulls %<>% separate(.Cinfo, c('Status', 'Regiao', 'Family', 'Genero', 'Especie'), sep = "\\.")
+  hulls$Family <- factor(hulls$Family, levels = unique(current.data$All$info$Familia))
+  
+  points.info <- ddply(plot.W, .(.Cinfo), numcolwise(mean))
+  points.info %<>% separate(.Cinfo, c('Status', 'Regiao', 'Family', 'Genero', 'Especie'), sep = "\\.")
+  
+  
+  pc_plot <- ggplot(plot.W, aes(PC1, PC4)) +
+    geom_polygon(aes(hPC1, hPC4, fill = Family, color = Family, group= Especie, log = "x"), 
+                 data = hulls, alpha=0.2) + 
+    geom_point(data = points.info,
+               aes (PC1, PC4, group= Genero, shape = interaction(Status, Regiao), color = Family, log = "x"), 
+               alpha = 0.7, size = 7) + 
+    #theme(legend.text = element_text(angle = 90, size = 10, hjust = 3, vjust = 3, face = 'italics')) +
+    scale_shape(name = "Localização e status", labels = c("Madagascar - viventes", "Madagascar - Extintos", "Fora de Madagascar - Viventes")) +
+    geom_text(data = ddply(points.info, .(Genero), numcolwise(mean)),
+              aes(PC1, PC4, label= Genero, log = "x"),  alpha = 0.4 ) +
+#        scale_size(name = "Size reference\nAverage PC1 score per genus", breaks = c(5.2, 5.6, 6.0),
+#                   labels = c("Small", "Medium", "Large") ) +
+    theme_bw() + 
+       theme(axis.title.x = element_text(face="bold", size=15) ) + labs(x = "PC1 = Tamanho") +
+       theme(axis.title.y = element_text(face="bold", size=15) ) + labs(y = "PC4 = Contraste entre comprimento\nda face e volume do neurocrânio ") +
+    ggtitle("Projeção no Morfoespaço da Matriz Ancestral") +
+    #coord_cartesian(ylim=c(-2, 58), xlim = c(-10, 580)) +
+    theme(plot.title = element_text(lineheight=.8, size = 20, face="bold")) + 
+    coord_fixed() +
+    guides(size = FALSE, 
+           colour = FALSE, 
+           fill = guide_legend(title.position = "top", title = "Família", size = 40, override.aes = list(alpha = 1) ),
+           shape = guide_legend(title.position = "top", override.aes = list(alpha = 1, size = 5), nrow = 1) ) +
+    theme(legend.position = 'bottom')
+  pc_plot
+  
+  pc_plot  + coord_trans(x = "log10", y = "log10")
+  pc_plot + coord_fixed()
+
+
+
+  
+#################################################################################################################
+# INTERPRETANDO OS PCs DA W
+#################################################################################################################
+#Escolha a matriz que voce quer, meu bem:
+plot(pruned.tree)
+nodelabels()
+Wmat <- Ancestral.Matrices$"42" 
+Wmat <- GeralMorphoSpace
+WPCs <- as.matrix(eigen(Wmat)$vectors)
+dimnames(WPCs)[[2]] <- paste("PC", 1:39, sep= "")
+dimnames(WPCs)[[1]] <- dimnames(all.main.data$All$matrix$cov)[[1]]
+
+m.WPCs <- melt(WPCs)
+m.WPCs$Var1<- factor( m.WPCs$Var1, levels = levels( m.WPCs$Var1)[39:1])
+m.WPCs.position =  m.WPCs
+m.WPCs.position$Var1 <- as.numeric( m.WPCs.position$Var1)
+m.WPCs.position$Var2 <- as.numeric( m.WPCs.position$Var2)
+m.WPCs.position$value= round( m.WPCs.position$value, 3)
+
+myPalette <- colorRampPalette(brewer.pal(11, 'Spectral')[-(4:6)], space = 'Lab')(n = 10)
+ggplot ( m.WPCs) +
+  geom_tile(aes(x = Var2, y = Var1, fill = value)) +
+  scale_fill_gradientn(name = '', colours = myPalette) +
+  ylab ('') + xlab ('') + labs(title = "GeralMorphoSpace PCs com fosseis") + 
+  geom_text(data =  m.WPCs.position, size = 3, aes(x = Var2, y = Var1, label = round(value, 2)) ) + 
+  theme(axis.text.x = element_text(angle = 90),
+        #axis.text.y = element_blank(),
+        axis.ticks = element_line(size = 0),
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size = 10),
+        rect = element_blank(), line = element_blank())
+#################################################################################################################
+
+KrzCor(cov.x = Ancestral.Matrices$"42" , cov.y = GeralMorphoSpace )
+RandomSkewers(cov.x = Ancestral.Matrices$"42" , cov.y = GeralMorphoSpace )
+
+
+
+## Canonical variates
+ProjetaDados = function(y,var.y){
+  y = as.matrix(y)
+  n = nrow(y)
+  p = ncol(y)
+  eigen.y = eigen(var.y)
+  eVal = eigen.y$values
+  eVec = eigen.y$vectors
+  Scores = array(0., c(n,p))
+  for( i in 1:n){
+    Scores[i,] = t(eVec)%*%(as.numeric(y[i,]))
+  }
+  for(i in 1:p){
+    Scores[,i] <- Scores[,i]/eVal[i]
+  }
+  return(Scores)
+}
+
+data_selected <- lin_data %>% 
+  select(., .Cinfo, Tombo, IS_PM:BA_OPI) %>% 
+  separate(., .Cinfo, c('Status', 'Regiao', 'Family', 'Genero', 'Especie'), sep = "\\." ) %>% 
+  filter ( Family == "Lemuridae") %>%
+  filter ( Genero == "Eulemur") %>%
+  mutate (., .Cinfo = paste(Status, Regiao, Family, Genero, Especie, sep = ".")) 
+data_selected$Tombo <- factor(x =data_selected$Tombo, levels = unique(lin_data$Tombo))
+rownames(data_selected) <- data_selected$Tombo
+
+#Escolha a matriz que voce quer, meu bem:
+plot(pruned.tree)
+nodelabels()
+Wmat <- Ancestral.Matrices$"65" 
+
+lm.within = lm(as.matrix(select(data_selected, IS_PM:BA_OPI)) ~ select(data_selected, Especie)[,1])
+
+current.data_projected_W = cbind(select(data_selected, .Cinfo, Tombo),  
+                                 ProjetaDados(select(data_selected, IS_PM:BA_OPI), Wmat))
+
+Bmat = cov(daply(na.omit(current.data_projected_W), .(.Cinfo), function(x) colMeans(x[,-c(1, 2)])))
+
+resp <- cbind(select(data_selected, .Cinfo, Especie, Tombo),  scale(ProjetaDados(select(data_selected, IS_PM:BA_OPI), Wmat) %*% eigen(Bmat)$vectors[,1:4], scale = TRUE))
+resp <- resp[!is.na(resp[,3]),]
+names(resp) <- c(".Cinfo", ".sp", "Tombo", "CV1", "CV2", "CV3", "CV4")
+table(rownames(resp) == resp$Tombo) 
+resp %<>% mutate(., .Cinfo = paste (.Cinfo, Tombo, sep = ".") )
+
+hulls <-ddply(resp, .(.Cinfo), plyr::summarise, "hpc1"=CV1[chull(CV1,CV2)],
+              "hpc2"=CV2[chull(CV1,CV2)])
+hulls %<>% separate(.Cinfo, c('Status', 'Regiao', 'Family', 'Genero', 'Especie', 'Tombo'), sep = "\\.")
+rownames(hulls) <- hulls$Tombo
+resp %<>% ddply(., .(.Cinfo), numcolwise(mean))
+resp %<>% separate(.Cinfo, c('Status', 'Regiao', 'Family', 'Genero', 'Especie', 'Tombo'), sep = "\\.")
+
+cv_plot_12 <- ggplot(resp, aes(CV1, CV2)) +
+  geom_polygon(aes(hpc1, hpc2, fill = Especie, group= Especie), hulls, alpha=.3) + 
+  geom_point(data = resp,
+             aes(CV1, CV2, group= Especie, color = Especie, shape = Genero, size= Status ), size = 5, alpha = 0.5) +
+  geom_text(data = hulls,
+             aes(hpc1, hpc2, group= Especie, label = Tombo), size = 5, alpha = 0.5) +
+  theme_bw() +
+  ggtitle("Canonical Variates") +
+  theme(plot.title = element_text(lineheight=.8, face="bold")) 
+cv_plot_12
+
+
+
+shape.plot[[1]]
+shape.plot + 
---
+++ R/Chapter 2/DriftTest.R
@@ -0,0 +1,163 @@
+
+#Teste na raiz apenas, usando a matriz ancestral:
+### nao roda pq? Error in lm.fit(x, y, offset = offset, singular.ok = singular.ok, ...) : 0 (non-NA) cases
+DriftTest(means = means.All.Fuckers, cov.matrix = Wmat.All.Fuckers, show.plot = TRUE) 
+#rodam, e rejeita em todos:
+DriftTest(means = means.madagascar.Fuckers, cov.matrix = Wmat.madagascar.Fuckers, show.plot = TRUE)
+DriftTest(means = means.extant.madagascar.Fuckers, cov.matrix = Wmat.extant.madagascar.Fuckers, show.plot = TRUE)
+DriftTest(means = means.extant.madagascar.Fuckers, cov.matrix = Wmat.madagascar.Fuckers, show.plot = TRUE)
+
+current.data<- sp.main.data
+
+# selecting parts of the main.data.list to perform the matrices comparrison and drift test 
+cov.mx <- current.data %>% llply(function(x) x$matrix$cov)
+mx.rep <- current.data %>% ldply(function(x) x$Mx.Rep$BootsRep[1]) 
+n.size <- current.data %>% ldply(function(x) x$sample.size) 
+ed.means <- current.data %>% ldply(function(x) x$ed.means) 
+gm.mean <- current.data %>% ldply(function(x) x$gm.mean) 
+
+# creating the masks to index the current set of matrices, means and repetabilities
+mask.n.size <- n.size[,2]>40
+mask.na.cov <- ldply(cov.mx, function(x) !is.na(x[1]))[,2]
+mask.rep <- !is.na(mx.rep)[,2]
+#Selecting accordingly to masks
+###Sample sizes #####
+sample.no.na <- n.size[mask.na.cov,2]
+names(sample.no.na) <- n.size[,1][mask.na.cov]
+##### Cov mx ########
+cov.no.na <- cov.mx[mask.na.cov]
+##### Mx repetabilities via RS ######
+rep.no.na <- mx.rep[mask.na.cov,2]
+names(rep.no.na) <- mx.rep[,1][mask.na.cov]
+##### ED means ######
+ed.means.no.na <- ed.means[mask.na.cov]
+gm.mean.no.na <- gm.mean [mask.na.cov, 2]
+# Árvore James: Personal comunication
+#I recommend the first phylogeny, fbd421agerange.tre, as the 'best' summary tree. 
+#You'll see that in fbd369agerange.tre, Plesiopithecus teras sneaks into lemurs as sister to Daubentonia, 
+#and this affects the age estimates. The phylogenies from tip-dating (td in file name) are much older than 
+#expected and may not be the best estimates. 
+
+treefile = read.nexus(file = "~/ataches/fbd369agerange_gooddates.tre")
+treefile = read.nexus(file = "attaches/fbd421agerange_edited.tre")
+species <- treefile$tip.label[treefile$tip.label %in% names(sample.no.na)]
+pruned.tree<-drop.tip(treefile,treefile$tip.label[-match(species, treefile$tip.label)])
+plot(pruned.tree)
+
+tree.drift.test<- TreeDriftTest(tree = pruned.tree, mean.list = ed.means.no.na , cov.matrix.list = cov.no.na, sample.sizes = sample.no.na)
+results <- llply(tree.drift.test, function(x) x$drift_rejected)
+PlotTreeDriftTest(test.list = tree.drift.test, tree = pruned.tree)
+nodelabels()
+tree.drift.test$`45`
+
+######### changing plot 
+tested.nodes <- as.numeric(names(tree.drift.test))
+non.drift.nodes <- laply(tree.drift.test, function(x) x$drift_rejected)
+i.c.5 <- ldply(tree.drift.test, function(x) x$coefficient_CI_95[2,])
+i.c.5<- ldply(tree.drift.test, function(x) x$coefficient_CI_95[1,])
+names(i.c.5) <- c('node', 'min', 'max')
+names(intercept.ic) <- c('node', 'min', 'max')
+i.c.5$min <- round(i.c.5$min, digits=2)
+i.c.5$max <- round(i.c.5$max, digits=2)
+
+# par(mfrow = c(1,1))
+# par(oma=c(1,1,1,1))
+# par(mar=c(0,2,1,1)+0.1)
+plot(pruned.tree, font = 3, label.offset = 15, no.margin = T, edge.color = "grey80", edge.width = 5, cex= 1.1)
+#nodelabels(node = tested.nodes, thermo = as.numeric(non.drift.nodes))
+i.c.5 <- ldply(tree.drift.test, function(x) x$coefficient_CI_95[2,])
+names(i.c.5) <- c('node', 'min', 'max')
+names(intercept.ic) <- c('node', 'min', 'max')
+i.c.5$min <- round(i.c.5$min, digits=2)
+i.c.5$max <- round(i.c.5$max, digits=2)
+nodelabels(node = tested.nodes , text = i.c.5$min, adj = 1.2, bg = "transparent", col = 'red', frame = "n", cex = i.c.5$min)
+nodelabels(node = tested.nodes, i.c.5$max, adj = -0.2, bg = "transparent", col = 'blue', frame = "n", cex = i.c.5$max)
+nodelabels(node = tested.nodes, pch = (as.numeric(non.drift.nodes)+17), cex=1.4, col = (as.numeric(non.drift.nodes)+5) )
+compare.stuff<- cbind( n.size[n.size[,1] %in% pruned.tree$tip.label, ], gm.mean[gm.mean[,1] %in% pruned.tree$tip.label, 2])
+names(compare.stuff) <- c('Especie', 'n', 'gm')
+compare.stuff$Especie <- as.character(compare.stuff$Especie)
+compare.stuff<- compare.stuff[with(compare.stuff, order(Especie)), ]
+tiplabels(pch = 21, cex =compare.stuff$n/18, adj = 6)
+tiplabels(pch = 15, cex = compare.stuff$gm/9, adj = 10)
+nodelabels(node = tested.nodes, col = 'grey30', adj = c(0.5, 1.25), cex = 0.5, frame = "n")
+# compare.stuff$Especie <- compare.stuff[sort(compare.stuff[,1]),]
+
+par(mar=c(0,0,1,3)+0.1)
+plot(pruned.tree, font = 3, label.offset = -3, no.margin = T, edge.color = "grey80", edge.width = 5, cex= 1.1, direction ="leftwards", show.tip.label = T, col = "white")
+#nodelabels(node = tested.nodes, thermo = as.numeric(non.drift.nodes))
+i.c.5<- ldply(tree.drift.test, function(x) x$coefficient_CI_95[1,])
+names(i.c.5) <- c('node', 'min', 'max')
+names(intercept.ic) <- c('node', 'min', 'max')
+i.c.5$min <- round(i.c.5$min, digits=2)
+i.c.5$max <- round(i.c.5$max, digits=2)
+nodelabels(node = tested.nodes , i.c.5$min, adj = -0.2, bg = "transparent", col = 'red', frame = "n", cex = i.c.5$min*0.6)
+nodelabels(node = tested.nodes, i.c.5$max, adj = 1.2, bg = "transparent", col = 'blue', frame = "n", cex = i.c.5$max*0.6)
+nodelabels(node = tested.nodes, pch = (as.numeric(non.drift.nodes)+17), cex=1.4, col = (as.numeric(non.drift.nodes)+9) )
+tiplabels(pch = 19, cex = gm.mean.no.na/10, adj = -2.5)
+
+
+
+tree.drift.test$`47`
+
+nodelabels()
+
+
+mask.n.size <- n.size[,2]>40
+###Sample sizes #####
+sample.40 <- n.size[mask.n.size,2]
+names(sample.40) <- n.size[mask.n.size,1]
+##### Cov mx ########
+cov.40 <- cov.mx[mask.n.size]
+##### Mx repetabilities via RS ######
+rep.40 <- mx.rep[mask.n.size,2]
+names(rep.40) <- mx.rep[,1][mask.n.size]
+##### ED means ######
+ed.means.40 <- ed.means[mask.n.size]
+
+names(ed.means.40) == names(rep.40) == names(cov.mx[mask.n.size]) == mx.rep[,1][mask.n.size]
+
+species.40 <- treefile$tip.label[treefile$tip.label %in% names(sample.40)]
+pruned.tree.40<-drop.tip(treefile,treefile$tip.label[-match(species.40, treefile$tip.label)])
+plot(pruned.tree.40)
+
+tree.drift.test.40<- TreeDriftTest(tree = pruned.tree.40, mean.list = ed.means.40 , cov.matrix.list = cov.40, sample.sizes = sample.40)
+results <- llply(tree.drift.test, function(x) x$drift_rejected)
+PlotTreeDriftTest(test.list = tree.drift.test.40, tree = pruned.tree.40)
+nodelabels()
+
+tree.drift.test<- TreeDriftTest(tree = pruned.tree, mean.list = ed.means.no.na , cov.matrix.list = cov.no.na, sample.sizes = sample.no.na)
+tree.drift.test$`62`
+
+DriftTest<- function (means, cov.matrix, show.plot = TRUE) 
+{
+  if (is.data.frame(means) | (!is.array(means) & !is.list(means))) 
+    stop("means must be in a list or an array.")
+  if (!isSymmetric(cov.matrix)) 
+    stop("covariance matrix must be symmetric.")
+  if (is.list(means)) {
+    mean.array <- laply(means, identity)
+  }
+  else {
+    mean.array <- means
+  }
+  W.pc <- eigen(cov.matrix)
+  projection.Wpc <- as.matrix(mean.array) %*% W.pc$vectors
+  log.B_variance <- log(apply(projection.Wpc, 2, var))
+  log.W_eVals <- log(W.pc$values)
+  regression <- lm(log.B_variance ~ log.W_eVals)
+  reg.plot <- ggplot(data.frame(log.B_variance, log.W_eVals, 
+                                names = 1:(dim(mean.array)[2])), aes_string("log.W_eVals", 
+                                                                            "log.B_variance")) + geom_text(aes_string(label = "names", size = 5)) + 
+    geom_smooth(method = "lm", color = "black") + labs(x = "log(W Eigenvalues)", 
+                                                       y = "log(B variances)") + theme_bw()
+  if (show.plot) 
+    print(reg.plot)
+  containsOne <- function(x) ifelse(x[1] < 1 & x[2] > 1, TRUE, 
+                                    FALSE)
+  test <- !containsOne(confint(regression)[2, ])
+  names(test) <- "5 %"
+  objeto <- list(regression = regression, coefficient_CI_95 = confint(regression), 
+                 log.between_group_variance = log.B_variance, log.W_eVals = log.W_eVals, 
+                 drift_rejected = test, plot = reg.plot)
+  return(objeto)
+}
