e1db185b2c56cb45a60fa8e09a4260571ef89bb9
Anna Penna <anna.penna@usp.com>
2016-03-27 03:07:30 -0300

    Proa Simulations

---
+++ R/Chapter 2/ProaSimulate.R
@@ -0,0 +1,74 @@
+#Drift test dos dois jeitos, com 3 conjudos de dados  #vai porra!
+
+cov.mx<- sp.main.data %>% llply(function(x) x$matrix$cov) # todas as matrizes de cov
+mask.extant <- ldply(sp.main.data, function(x) unique(x$info$Status) == "Extant")[,2] #mascara para os vventes
+mask.is.na.cov <- ldply(cov.mx, function(x) is.na(x[1]))[,2] # mascara dos sem matriz
+mask.no.na.cov <- ldply(cov.mx, function(x) !is.na(x[1]))[,2] #mascara dos com matriz, mesmo que seja tosca
+mask.madagascar <- ldply(sp.main.data, function(x) unique(x$info$Regiao) == "Madagascar")[,2] 
+n.size <- sp.main.data %>% ldply(function(x) x$sample.size) %>% .[,2] #tamanho amostral de todos
+mask.n.size <- n.size> 30 # mascara de tamanho amostral >30
+ed.means <- sp.main.data %>% llply(function(x) x$ed.means) #médias de todos
+mask.madagascar & mask.no.na.cov
+
+B.var <- sp.main.data[mask.madagascar & mask.no.na.cov] %>% ldply(function(x) x$ed.means) %>% .[, -1] %>% var
+
+B.all <- sp.main.data[mask.madagascar & mask.no.na.cov] %>% ldply(function(x) x$ed) 
+
+B.all.lm <- lm(as.matrix(B.all[,-1]) ~ B.all[, 1])
+B.all.lm.1 <- lm(as.matrix(B.all[,-1]) ~ 1)
+
+sumsqr.W <- t(B.all.lm$residuals) %*% B.all.lm$residuals 
+W.lm <- CalculateMatrix(B.all.lm)
+W.lm - Ancestral.Matrices$`45`
+sumsqr.W.t <- t(B.all.lm.1$residuals) %*% B.all.lm.1$residuals 
+
+sp.main.data[mask.madagascar & mask.no.na.cov] %>% ldply(function(x) x$ed) %>% dim
+
+B.sumsqr <- (sumsqr.W.t - sumsqr.W) / 1318
+MatrixCompare(B.sumsqr, B.var)
+MatrixCompare(B.sumsqr, Ancestral.Matrices$`42`)
+
+Proa.1.GP <- simulateGP(method = "sim1", 
+                        m = dim(cov.mx[[1]])[1], 
+                        tNe = 0.001, 
+                        pop = length(n.size[mask]),
+                        n = 40,
+                        sim.n = 1000,
+                        G = B.sumsqr, 
+                        P = Ancestral.Matrices$`45`)
+
+Proa.2.GP <- simulateGP(method = "sim2", 
+                        m = dim(cov.mx[[1]])[1], 
+                        tNe = 0.001, 
+                        pop = length(n.size[mask]),
+                        n = 40,
+                        sim.n = 1000,
+                        G = B.sumsqr, 
+                        P = Ancestral.Matrices$`45`)
+
+Proa.3.GP <- simulateGP(method = "sim3", 
+                        m = dim(cov.mx[[1]])[1], 
+                        tNe = 0.001, 
+                        pop = length(n.size[mask]),
+                        n = 40,
+                        sim.n = 1000,
+                        G = B.sumsqr, 
+                        P = Ancestral.Matrices$`45`)
+Proa.4.GP <- simulateGP(method = "sim4", 
+                        m = dim(cov.mx[[1]])[1], 
+                        tNe = 0.001, 
+                        pop = length(n.size[mask]),
+                        n = 40,
+                        sim.n = 1000,
+                        G = B.sumsqr, 
+                        P = Ancestral.Matrices$`45`)
+Proa.5.GP <- simulateGP(method = "sim5", 
+                        m = dim(cov.mx[[1]])[1], 
+                        tNe = 0.001, 
+                        pop = length(n.size[mask]),
+                        n = 40,
+                        sim.n = 1000,
+                        G = B.sumsqr, 
+                        P = Ancestral.Matrices$`45`)
+
+Proa.1.Beta <- simulateGPBeta(method = "sim1", m = dim(cov.mx[[1]])[1], tNe = 60000, pop = length(cov.mx[mask]) , n = n.size[mask], sim.n = 1000, G = B.sumsqr, P = Ancestral.Matrices$`42`)
---
+++ R/Chapter 2/ProaSimulationsFunctions.R
@@ -0,0 +1,186 @@
+#### from Proa et al, Evolution, 2013:
+# TYPE I ERROR RATES FOR TESTING GENETIC DRIFT WITH PHENOTYPIC COVARIANCE MATRICES: A SIMULATION STUDY
+
+simulateGP<-function(method, m, tNe, pop, n, sim.n, G="NULL", P="NULL")
+  {
+  #method = choose "sim1", "sim2", "sim3", "sim4", "sim5", according to description in manuscript and bellow
+  #m = genetic covariance matrix dimension
+  #tNe = number of generations (t) divided by effective population size (Ne)
+  #pop = number of populations
+  #n = sample size for each population
+  #sim.n = number of simulations
+
+  
+  #define an empty variable to contain the results
+  beta <- vector()
+  prob <- vector()
+  require(MASS)
+  require(clusterGeneration)
+  
+  for (i in 1:sim.n) 
+  {
+    if (method=="sim1")
+    {
+      if (G=="NULL") 
+      {
+        stop("you need to provide a valid G matrix!\n") 
+      }
+      if (P=="NULL") {stop("you need to provide a valid P matrix!\n") 
+      }
+      #Predetermined G and P will be used 
+      ###### segundo o artigo é meter a W no lugar da P e a B no lugar da G
+    }
+    if (method=="sim2")
+    {
+      #G is defined as a random positive definite matrix, and P = xG, only different by a random constant of proportionality.
+#       if (G=="NULL") 
+#       {
+#         stop("you need to provide a valid G matrix!\n") 
+#       }
+#       
+      G <- genPositiveDefMat(dim=m,lambdaLow=1,ratioLambda=10)$Sigma
+      P <- runif(1,min=1,max=10)*G
+    }
+    if (method=="sim3")
+    {
+      #G and P matrices are defined randomly and independently from each other, with the constraint that the variances in P are always larger than the respective variances in G.
+      G <- genPositiveDefMat(dim = m, covMethod = "unifcorrmat", rangeVar = c(1,10))$Sigma
+      R <- G + genPositiveDefMat(dim = m, covMethod = "unifcorrmat", rangeVar = c(1,15))$Sigma
+      var<-runif(m,2,9)*diag(G)
+      P<-diag(sqrt(var))%*%R%*%diag(sqrt(var))
+    }
+    if (method=="sim4")
+    {
+      #G and E matrices are defined randomly and independently from each other, whereas P = G+E. P and G do not share a common PC structure but are related
+      G <- genPositiveDefMat(dim = m, covMethod = "unifcorrmat", rangeVar = c(1,10))$Sigma
+      P <- G + genPositiveDefMat(dim = m, covMethod = "unifcorrmat", rangeVar = c(1,15))$Sigma
+    }
+    if (method=="sim5")
+    {
+      #G and E matrices are defined randomly and independently from each other, whereas P = G+E. P and G share a common PC structure
+      G <- genPositiveDefMat(dim=m, lambdaLow=1, ratioLambda=10)$Sigma
+      P <- G + genPositiveDefMat(dim=m, covMethod="unifcorrmat", rangeVar = c(1,5))$Sigma
+    }
+    #generate samples from pop populations with n obs each. the ancestral vector is composed of m zeros.
+    M <-mvrnorm(pop, rep(0, ncol(G)), tNe*G)
+    group <- factor(rep(seq(1:pop),each=n))
+    data <- matrix(0, pop*n, ncol(P) )
+    for (j in 1: (n*pop) )
+    {
+      data[j,] <- mvrnorm(1, M[group[j],], P)
+    }
+    
+    #calculate matrix of mean vectors from simulations
+    
+    zmeans <- matrix(unlist(by(data, group, colMeans)),  nrow=pop, ncol = ncol(G), byrow=T)
+    
+    #calculate within-group phenotypic covariance and extract eigenvalues
+    
+    eigW <- eigen(cov( data - zmeans[rep(1:nrow(zmeans), each = n),]))
+    
+    #Project mean vectors for the pop pops on within group eigenvectors
+    
+    zm.proj <- zmeans %*% eigW$vectors
+    
+    #calculate among-group variance
+    
+    v <- diag(cov(zm.proj))
+    
+    #perform Ackermann and Cheverud test with t-test for unity slope
+    
+    model <- summary(lm(log(v) ~ log(eigW$values)))
+    prob[i] <- pt(abs((model$coefficients[2,1]-1) / model$coefficients[2,2]), df = model$df[2], lower.tail=F)*2
+    beta[i] <- model$coefficients[2,1]
+  }
+  #Calculate 95% confidence limits
+  Beta <- vector("list", 3)
+  Beta$UL<-mean(beta)+1.96*sd(beta)
+  Beta$LL<-mean(beta)-1.96*sd(beta)
+  Beta$mean.beta <- mean(beta)
+    
+  tIe <- mean(prob < 0.05)
+  
+  return(list("TypeIerror" = tIe,
+              "Probabilities" = prob),
+              "Beta" = Beta)
+}
+
+
+
+simulateGPBeta<-function(method, m, tNe, pop, n, sim.n, G="NULL", P="NULL",smax){
+  #define an empty variable to contain the results
+  beta <- vector()
+  require(MASS)
+  require(clusterGeneration)
+  
+  for (i in 1:sim.n) 
+  {
+    if (method=="sim1")
+    {
+      if (G=="NULL") 
+      {
+        stop("you need to provide a valid G matrix!\n") 
+      }
+      if (P=="NULL") {stop("you need to provide a valid P matrix!\n") 
+      }
+      #Predetermined G and P will be used
+    }
+    if (method=="sim2")
+    {
+      #G is defined as a random positive definite matrix, and P = xG, only different by a random constant of proportionality.
+      G<-genPositiveDefMat(dim=m,lambdaLow=1,ratioLambda=10)$Sigma
+      P<-runif(1,min=1,max=10)*G
+    }
+    if (method=="sim3")
+    {
+      #G and P matrices are defined randomly and independently from each other, with the constraint that the variances in P are always larger than the respective variances in G.
+      G<-genPositiveDefMat(dim=m,covMethod="unifcorrmat",rangeVar=c(1,10))$Sigma
+      R<-genPositiveDefMat(dim=m,covMethod="unifcorrmat",rangeVar=c(1,1))$Sigma
+      var<-runif(m,2,9)*diag(G)
+      P<-diag(sqrt(var))%*%R%*%diag(sqrt(var))
+    }
+    if (method=="sim4")
+    {
+      #G and E matrices are defined randomly and independently from each other, whereas P = G+E. P and G do not share a common PC structure but are related
+      G<-genPositiveDefMat(dim=m,covMethod="unifcorrmat",rangeVar=c(1,10))$Sigma
+      P<-G+genPositiveDefMat(dim=m,covMethod="unifcorrmat",rangeVar=c(0,smax))$Sigma
+    }
+    if (method=="sim5")
+    {
+      #G and E matrices are defined randomly and independently from each other, whereas P = G+E. P and G share a common PC structure
+      G<-genPositiveDefMat(dim=m,lambdaLow=1,ratioLambda=10)$Sigma
+      P<-G+genPositiveDefMat(dim=m,covMethod="unifcorrmat",rangeVar=c(0,smax))$Sigma
+    }
+    
+    #generate samples from pop populations with n obs each. the ancestral vector is composed of m zeros.
+    M<-mvrnorm(pop,rep(0,ncol(G)),tNe*G)
+    group<-factor(rep(seq(1:pop),each=n))
+    data <- matrix(0,pop*n,ncol(P))
+    for (j in 1:(n*pop)){data[j,]<-mvrnorm(1,M[group[j],],P)}
+    
+    #calculate matrix of mean vectors from simulations
+    
+    zmeans<-matrix(unlist(by(data,group,colMeans)),nrow=pop,ncol=ncol(G),byrow=T)
+    
+    #calculate within-group phenotypic covariance and extract eigenvalues
+    
+    eigW<-eigen(cov(data-zmeans[rep(1:nrow(zmeans),each=n),]))
+    
+    #Project mean vectors for the 15 pops on within group eigenvectors
+    
+    zm.proj<-zmeans%*%eigW$vectors
+    
+    #calculate among-group variance
+    
+    v<-diag(cov(zm.proj))
+    
+    #calculate slope of Ackermann and Cheverud test
+    
+    model<-summary(lm(log(v)~log(eigW$values)))
+    beta[i]<-model$coefficients[2,1]
+  }
+  #Calculate 95% confidence limits
+  UL<-mean(beta)+1.96*sd(beta)
+  LL<-mean(beta)-1.96*sd(beta)
+  return(c(mean(beta),UL,LL))
+}
