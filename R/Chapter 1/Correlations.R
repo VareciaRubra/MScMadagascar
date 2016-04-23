dec3ee16c52e466037fc0a8b3d43505bdecf4748
Anna Penna <anna.penna@usp.com>
2016-04-23 12:23:05 -0300

    using species with hight samples only

--- R/Chapter 1/Correlations.R
+++ R/Chapter 1/Correlations.R
@@ -16,7 +16,7 @@ mean.sd<-function(x) {
 
 mean.sim(abs(sp.main.data$Tarsius_bancanus$matrix$cor))
 
-Moduling <- sp.main.data[mask.no.na.cov] %>% 
+Moduling <- sp.main.data[mask.n.size] %>% 
   ldply( function(x) data.frame( "trait" =  names(mean.sim(x$matrix$cor)), "mean.cor" = mean.sim(abs(x$matrix$cor)), "sd.cor" = mean.sd(abs(x$matrix$cor)) ) , .progress = "text" )
 
 Moduling$cv <- Moduling$sd.cor/ Moduling$mean.cor
@@ -26,33 +26,47 @@ Moduling$.id <- factor(Moduling$.id, levels = rev(unique(Moduling$.id)) )
 Moduling$r2 <- 
   rep(r2.stuff$mean, each = 39)
 
-modules <- as.matrix(apply(Modular.hyp,2, FUN = rep, 42))
+Moduling$gm <- 
+  rep(gm.mean$V1, each = 39)
+dimnames(Modular.hyp)
+unique(Moduling$trait)
+
+modules <- as.matrix(apply(Modular.hyp,2, FUN = rep, dim(Moduling)[1]/39))
 dimnames(modules)
 modules[,1]  
 Moduling <- cbind(Moduling, modules)
 Moduling$Neuro
 
 names(Moduling)[1] <- "sp"
-Moduling %>%   
-ggplot( aes(x = mean.cor, y = sd.cor, color = trait)) +
-  geom_smooth(method = "lm", se = F, aes(x = mean.cor, y = sd.cor, group = interaction (trait))) +
+Moduling %>% filter (sp != "Euoticus_elegantulus") %>% 
+  filter (sp != "Prolemur_simus") %>%
+ggplot( aes(x = mean.cor, y = sd.cor)) +
+  geom_point() +
+  geom_text(aes(label = sp))+
+  geom_smooth(method = "lm", se = F, aes(x = mean.cor, y = sd.cor, color = interaction (Face, Neuro), group = interaction (trait))) +
   facet_wrap(~trait, ncol = 8) +
   theme(legend.position = "none")
 
-Moduling %>% melt %>% filter (variable == "cv") %>%
-  ggplot( aes(y = trait, x = value)) +
-  geom_violin() +
-  geom_jitter(aes(color = as))
-  geom_boxplot()+
-  facet_wrap(~sp)
+Moduling %>% 
+  ggplot(.) +
+  geom_density(aes(x= cv, group = trait)) +
+  geom_histogram(aes(x= cv, group = trait, color = Base)  )  +
+  facet_wrap(~trait)+ theme_bw()
+
+Moduling %>% filter (Face == 1) %>%
+ggplot(., aes(y = cv, x = trait)) +
+  geom_jitter(aes(color = sp))+
+  #geom_dotplot(stackdir = "up", stackratio = 0.3)+
+  #facet_wrap(~trait) + 
+  guides(color = guide_legend(ncol =1))
 
 Moduling %>% #filter (trait == "BA_OPI") %>%
   ggplot( aes(y = r2, x = cv)) +
-  geom_point(aes(color = interaction (as.factor(Base), as.factor(Vault)) )) +
+  geom_point(aes(color = interaction (as.factor(Face), as.factor(Neuro)) )) +
   #geom_text(aes(label = trait, color = as.factor(Vault)) )+
   #geom_smooth(method = "lm", se = F, aes(x = cv, y = mean.cor, group = interaction (trait))) +
   theme_bw() + 
-  facet_wrap(~trait, ncol = 8) + 
+  facet_wrap(~trait, ncol = 8) 
 
 
 r2.stuff
